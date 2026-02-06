0fb8bc0b6731f8958379091e3031d002f510adbe
Beck <beckzod1993@gmail.com>
2026-02-06 19:52:13 +0600

    added new website components

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,50 @@
 window.addEventListener('scroll', function (){
    document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY> 137);
 });
+
+const offcanvasCartEl = document.getElementById('offcanvasCart');
+const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+
+document.querySelectorAll('.closecart').forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault;
+        offcanvasCart.hide();
+        let href = item.dataset.href;
+        offcanvasCartEl.addEventListener('hidden.bs.offcanvas', () => {
+            document.getElementById(href).scrollIntoView();
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    }); 
+
+    $('#top').click(function() {
+        $('html,body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+    $(".owl-carousel").owlCarousel({
+    margin:20,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+  });
+});
